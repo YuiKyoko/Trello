ad277e85f8dbd9a987fd306c35c239e402de9831
Kimberly <kimberlyesquivelgala@gmail.com>
2016-09-23 08:35:08 -0500

    v0.0.4

--- js/main.js
+++ js/main.js
@@ -1,9 +1,61 @@
-window.addEventListener("load",function () {
-	var oculto=document.getElementById("oculto");
-	var mostrar=document.getElementById("formulario");
+window.addEventListener("load",function(){
+    var span = document.getElementById("span");
+    var boton = document.getElementById("boton");
+    var texto = document.getElementById("texto");
+    var formulario= document.getElementById("formulario");
+    var form = document.getElementById("form");
+    var contenedor = document.getElementById("contenedor");
+    
+    span.addEventListener("click",function(e){
+        e.preventDefault();
+        mostrarFormulario();
+        texto.focus();
+    });
 
-	oculto.addEventListener("click", function () {
-		oculto.style.display="none";
-		mostrar.style.display="block";
-	});
+    boton.addEventListener("click",function(e){
+        e.preventDefault();
+        mostrarLista();
+        texto.value = "";
+    });
+
+    function mostrarFormulario(){
+        formulario.style.display = "inline-block";
+        span.style.display = "none";
+        form.style.display = "inline-block";
+    }
+    function mostrarLista(){
+        formulario.style.display = "none";
+        span.style.display = "inline-block";
+        var lista = document.createElement("div");
+        var nodo = document.createTextNode(texto.value);
+        lista.classList.add("text");
+        lista.appendChild(nodo);
+        lista.classList.add("lista");
+        contenedor.insertBefore(lista,form.previousSibling);
+        lista.style.display = "inline-block";
+        lista.style.cssFloat = "left";
+
+        var tarjetaNueva = document.createElement("a");
+        tarjetaNueva.href= "#";
+        var tarjetaNodo =document.createTextNode("Añadir una tarjeta ...")
+        tarjetaNueva.appendChild(tarjetaNodo);
+        lista.appendChild(tarjetaNueva);
+        tarjetaNueva.classList.add("tarjeta");
+
+        tarjetaNueva.addEventListener("click", function(){
+        tarjetaNueva.style.display = "none";
+
+        var area = document.createElement("textarea");
+        area.classList.add("textS");
+        lista.appendChild(area);
+        lista.insertBefore(area,nodo.nextSibling);
+
+        var btn = document.createElement("button");
+        var btnNodo = document.createTextNode("Añadir");
+        btn.classList.add("btn");
+        btn.appendChild(btnNodo);
+        lista.appendChild(btn);
+        lista.insertBefore(btn,area.nextSibling);
+        });
+    }
 });
