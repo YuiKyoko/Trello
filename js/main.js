70ea5878543ce7d654ab9a244b1c6e2cb3991158
YuiKyoko <kimberlyesquivelgala@gmail.com>
2016-10-11 17:35:56 -0500

    mejorando

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@ window.addEventListener("load",function(){
     boton.addEventListener("click",function(e){
         e.preventDefault();
         mostrarLista();
+        //mostrarTarjeta();
         texto.value = "";
     });
 
@@ -43,19 +44,70 @@ window.addEventListener("load",function(){
         tarjetaNueva.classList.add("tarjeta");
 
         tarjetaNueva.addEventListener("click", function(){
-        tarjetaNueva.style.display = "none";
+            this.style.display = "none";
 
-        var area = document.createElement("textarea");
-        area.classList.add("textS");
-        lista.appendChild(area);
-        lista.insertBefore(area,nodo.nextSibling);
+            var area = document.createElement("textarea");
+            area.classList.add("textS");
+            lista.appendChild(area);
+            lista.insertBefore(area,nodo.nextSibling);
+
+            var btn = document.createElement("button");
+            var btnNodo = document.createTextNode("Añadir");
+            btn.classList.add("btn");
+            btn.appendChild(btnNodo);
+            btn.addEventListener("click", mostrarTarjeta)
+
+            lista.appendChild(btn);
+            lista.insertBefore(btn,area.nextSibling);
+        });
+    }
+    function mostrarTarjeta(e){
+        var mostrarLista = e.target.parentElement;
+        var agregarTarjeta = document.createElement("a");
+        agregarTarjeta.href= "#";
+        var añadirTarjeta =document.createTextNode("Añadir una tarjeta ...")
+        agregarTarjeta.appendChild(añadirTarjeta);
+        mostrarLista.appendChild(agregarTarjeta);
+        agregarTarjeta.classList.add("tarjeta");
+
+        agregarTarjeta.addEventListener("click", function(){
+        agregarTarjeta.style.display = "none";
+
+        var area = document.createElement("text");
+        area.classList.add("inputS");
+        mostrarLista.appendChild(area);
+        mostrarLista.insertBefore(area,nodo.nextSibling);
 
         var btn = document.createElement("button");
         var btnNodo = document.createTextNode("Añadir");
         btn.classList.add("btn");
         btn.appendChild(btnNodo);
-        lista.appendChild(btn);
-        lista.insertBefore(btn,area.nextSibling);
+        mostrarLista.appendChild(btn);
+        mostrarLista.insertBefore(btn,area.nextSibling);
         });
     }
+
+    function start(e) {
+    e.dataTransfer.effecAllowed = 'move'; 
+    e.dataTransfer.setData("Text", e.target.id); 
+    e.target.style.opacity = '0.4'; 
+    }
+    function end(e){
+        e.target.style.opacity = '';       
+        e.dataTransfer.clearData("Data");           
+    }
+    function over(e) {
+        if ((e.target.className == "contenedorPieza") || (e.target.id == "formulario"))
+            return false;
+        else
+        return true;
+    }
+    function drop(e){
+        e.preventDefault(); 
+        var elementoArrastrado = e.dataTransfer.getData("Text");
+        e.target.appendChild(document.getElementById(elementoArrastrado));
+        comprobarPuzzle();
+    }
+
+
 });
